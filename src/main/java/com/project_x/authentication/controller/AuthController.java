686cc11e6f63c4bbfc2a95cb754154393091f27d
Bayonle010 <afeezonadiran@gmail.com>
2026-02-05 23:52:44 +0100

    update: validates registration request body

--- src/main/java/com/project_x/authentication/controller/AuthController.java
+++ src/main/java/com/project_x/authentication/controller/AuthController.java
@@ -3,13 +3,15 @@ package com.project_x.authentication.controller;
 import com.project_x.authentication.dto.request.RegistrationRequest;
 import com.project_x.authentication.service.AuthService;
 import com.project_x.core.response.ApiResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth/")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
     private  final AuthService authService;
 
@@ -18,7 +20,7 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse> registerUser(RegistrationRequest request){
+    public ResponseEntity<ApiResponse> registerUser( @Valid @RequestBody RegistrationRequest request){
         return authService.registerUser(request);
     }
 }
