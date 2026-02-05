ab7eb26c3f4ab5f7806ac2b897a09fffee8ba58b
Bayonle010 <afeezonadiran@gmail.com>
2026-02-05 23:02:41 +0100

    update: overides spring's default custom entry point

---
+++ src/main/java/com/project_x/core/security/CustomAuthEntryPoint.java
@@ -0,0 +1,39 @@
+package com.project_x.core.security;
+
+import com.project_x.core.response.ApiResponse;
+import com.project_x.core.response.ResponseUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public CustomAuthEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        ApiResponse apiResponse = ResponseUtil.error(
+                HttpServletResponse.SC_UNAUTHORIZED,
+                "Unauthorized",
+                authException.getMessage(),
+                null
+        );
+
+        objectMapper.writeValue(response.getWriter(), apiResponse);
+    }
+
+}
