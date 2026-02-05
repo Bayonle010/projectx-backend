e35b3f42575e4b6d60d741f47697478e289cbf0d
Bayonle010 <afeezonadiran@gmail.com>
2026-02-05 19:14:07 +0100

    update: encrypts password

--- src/main/java/com/project_x/authentication/service/impl/AuthServiceImpl.java
+++ src/main/java/com/project_x/authentication/service/impl/AuthServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -22,6 +23,13 @@ public class AuthServiceImpl implements AuthService {
 
     private final static Logger log = LoggerFactory.getLogger(AuthServiceImpl.class)
     private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public ResponseEntity<ApiResponse> registerUser(RegistrationRequest request) {
@@ -38,7 +46,7 @@ public class AuthServiceImpl implements AuthService {
 
 
         // Encode password
-        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        String encodedPassword = passwordEncoder.encode(request.password());
 
         // Create new user
         User newUser = User.builder()
