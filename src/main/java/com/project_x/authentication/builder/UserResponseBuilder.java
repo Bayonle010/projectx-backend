5ddc2277a7872aedbe99a88e062c592ca16650cb
Bayonle010 <afeezonadiran@gmail.com>
2026-02-05 19:31:32 +0100

    update: creates user response builder

---
+++ src/main/java/com/project_x/authentication/builder/UserResponseBuilder.java
@@ -0,0 +1,23 @@
+package com.project_x.authentication.builder;
+
+import com.project_x.role.Role;
+import com.project_x.user.entity.User;
+
+import java.util.List;
+
+public class UserResponseBuilder {
+    public static UserResponse toDto(User user){
+        List<String> roles = user.getRoles().stream().map(Role::getAuthority).toList();
+
+        return UserResponse.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .firstName(user.getFirstname())
+                .lastName(user.getLastname())
+                .isVerified(user.isEmailVerified())
+                .role(String.valueOf(roles))
+                .userType(String.valueOf(user.getUserType()))
+                .phoneNumber(user.getPhoneNumber())
+                .build();
+    }
+}
