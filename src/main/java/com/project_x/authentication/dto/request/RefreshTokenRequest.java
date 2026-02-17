e93296c106dba486228b5a5fdb6de7d576cf1e7b
Bayonle010 <afeezonadiran@gmail.com>
2026-02-17 18:17:20 +0100

    update: creates refresh token contract

---
+++ src/main/java/com/project_x/authentication/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,11 @@
+package com.project_x.authentication.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RefreshTokenRequest(
+        @NotBlank(message = "refreshtoken field cannot be blank")
+        @NotNull(message = "refreshtoken fields cannot be null")
+        String refreshToken
+) {
+}
--- src/main/java/com/project_x/authentication/service/AuthService.java
+++ src/main/java/com/project_x/authentication/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.project_x.authentication.service;
 
 import com.project_x.authentication.dto.request.LoginRequest;
+import com.project_x.authentication.dto.request.RefreshTokenRequest;
 import com.project_x.authentication.dto.request.RegistrationRequest;
 import com.project_x.core.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
@@ -8,4 +9,5 @@ import org.springframework.http.ResponseEntity;
 public interface AuthService {
     ResponseEntity<ApiResponse> registerUser(RegistrationRequest request);
     ResponseEntity<ApiResponse> authenticateUser(LoginRequest request);
+    ResponseEntity<ApiResponse> refreshToken(RefreshTokenRequest request);
 }
