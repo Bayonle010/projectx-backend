a64428a37c6eb7662c4100bdf72345fa2cada8cc
Bayonle010 <afeezonadiran@gmail.com>
2026-02-17 18:33:09 +0100

    update: exposes refresh token endpoint

--- src/main/java/com/project_x/authentication/controller/AuthController.java
+++ src/main/java/com/project_x/authentication/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.project_x.authentication.controller;
 
 import com.project_x.authentication.dto.request.LoginRequest;
+import com.project_x.authentication.dto.request.RefreshTokenRequest;
 import com.project_x.authentication.dto.request.RegistrationRequest;
 import com.project_x.authentication.service.AuthService;
 import com.project_x.core.response.ApiResponse;
@@ -28,5 +29,10 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> authenticate( @Valid @RequestBody LoginRequest request){
         return authService.authenticateUser(request);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<ApiResponse> authenticate( @Valid @RequestBody RefreshTokenRequest request){
+        return authService.refreshToken(request);
     }
 }
