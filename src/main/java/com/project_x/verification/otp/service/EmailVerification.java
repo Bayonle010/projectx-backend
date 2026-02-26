ccbb0b3f12a508f83a0edcf842adbcb0e48b5ffe
Bayonle010 <afeezonadiran@gmail.com>
2026-02-26 13:07:26 +0100

    update: creates template for 'afer email verification event

--- src/main/java/com/project_x/verification/otp/service/EmailVerification.java
+++ src/main/java/com/project_x/verification/otp/service/EmailVerification.java
@@ -3,9 +3,11 @@ package com.project_x.verification.otp.service;
 import com.project_x.core.response.ApiResponse;
 import com.project_x.verification.otp.dto.request.SignUpOtpRequest;
 import com.project_x.verification.otp.dto.request.VerifyOtpRequest;
+import com.project_x.verification.otp.entity.Otp;
 import org.springframework.http.ResponseEntity;
 
 public interface EmailVerification {
     ResponseEntity<ApiResponse> generateSignUpOtp(SignUpOtpRequest signUpOtpRequest);
     ResponseEntity<ApiResponse> verifyOtp(VerifyOtpRequest verifyOtpRequest);
+    ResponseEntity<ApiResponse> handleVerifySignUpOtp(Otp otp);
 }
--- src/main/java/com/project_x/verification/otp/service/impl/EmailVerificationImpl.java
+++ src/main/java/com/project_x/verification/otp/service/impl/EmailVerificationImpl.java
@@ -2,6 +2,7 @@ package com.project_x.verification.otp.service.impl;
 
 import com.project_x.core.response.ApiResponse;
 import com.project_x.core.response.ResponseUtil;
+import com.project_x.user.entity.User;
 import com.project_x.user.enums.UserType;
 import com.project_x.user.service.UserService;
 import com.project_x.verification.otp.dto.request.SignUpOtpRequest;
@@ -14,6 +15,11 @@ import com.project_x.verification.otp.service.OtpService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class EmailVerificationImpl implements EmailVerification {
@@ -56,12 +62,39 @@ public class EmailVerificationImpl implements EmailVerification {
         if (!result.isValid()) {
             return ResponseEntity
                     .status(result.httpStatus())
-                    .body(ResponseUtil.error(99, result.getErrorMessage(), result.getDetails(), null));
+                    .body(ResponseUtil.error(99, result.errorMessage(), result.details(), null));
         }
 
         Otp otp = result.otp();
         otpService.deleteOtp(otp);
 
         return handleVerifySignUpOtp(otp);
+    }
+
+
+    @Override
+    public ResponseEntity<ApiResponse> handleVerifySignUpOtp(Otp otp) {
+        User user = userService.findUserByEmail(otp.getOtpMedium());
+
+        if (user.isEmailVerified()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(ResponseUtil.error(99, "User Already verified", "", ""));
+        }
+
+
+        user.setEmailVerified(true); //Marked user as a verified user
+        userService.save(user);
+
+
+//        TODO: SEND WELCOME EMAIL/ OTHER THINGS
+//        try {
+//            List<Param> params = new ArrayList<>();
+//            params.add(Param.builder().name("first_name").value(user.getFirstname()).build());
+//            params.add(Param.builder().name("year").value(String.valueOf(LocalDate.now().getYear())).build());
+//            messagingHandler.sendEmailNotificationToQueue(List.of(user.getEmail()), new ArrayList<>(), new ArrayList<>(),
+//                    MessageType.Template, EmailTemplate.WELCOME_EMAIL_TEMPLATE, "WELCOME TO PROJECTX", "noreply@siryoungtech.com", params, true);
+//        } catch (Exception e) {
+//            logger.error(e.getMessage());
+//        }
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseUtil.success(0, "Email verification successful" , "","", null));
     }
 }
