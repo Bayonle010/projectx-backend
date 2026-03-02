c051809e8148f69c8ae1019ba8c25c76680e3cc4
Bayonle010 <afeezonadiran@gmail.com>
2026-03-02 17:44:08 +0100

    update: implments forgot password otp service

--- src/main/java/com/project_x/authentication/service/impl/ForgotPasswordImpl.java
+++ src/main/java/com/project_x/authentication/service/impl/ForgotPasswordImpl.java
@@ -3,11 +3,39 @@ package com.project_x.authentication.service.impl;
 import com.project_x.authentication.dto.request.ForgotPasswordOtpRequest;
 import com.project_x.authentication.service.ForgotPasswordService;
 import com.project_x.core.response.ApiResponse;
+import com.project_x.core.response.ResponseUtil;
+import com.project_x.user.enums.UserType;
+import com.project_x.user.service.UserService;
+import com.project_x.verification.otp.enums.OtpEvent;
+import com.project_x.verification.otp.service.OtpService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ForgotPasswordImpl implements ForgotPasswordService {
+
+    private final OtpService otpService;
+
+    public ForgotPasswordImpl(OtpService otpService) {
+        this.otpService = otpService;
+    }
+
+
     @Override
     public ResponseEntity<ApiResponse> handleSendForgotPasswordOtp(ForgotPasswordOtpRequest request) {
-        return null;
+        String formattedEmail = request.email().toLowerCase().trim();
+
+
+        boolean generateOtp = otpService.handleGenerateOtp(
+                formattedEmail, OtpEvent.FORGOT_PASSWORD, 300L, "PASSWORD RESET OTP",
+                UserType.USER, "forgot password : otp"
+        );
+
+        if (!generateOtp) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseUtil.error(99, "something went wrong", null, null));
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseUtil.success(0, "Success", String.format("OTP sent to %s ", formattedEmail), "", null));
     }
 }
