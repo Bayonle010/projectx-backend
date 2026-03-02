4e4fe29dc284abc9655016488c430f557aa4a7af
Bayonle010 <afeezonadiran@gmail.com>
2026-03-02 18:50:57 +0100

    update: exposes forgot password otp verification endpoint

--- src/main/java/com/project_x/authentication/controller/ForgotPasswordController.java
+++ src/main/java/com/project_x/authentication/controller/ForgotPasswordController.java
@@ -1,8 +1,10 @@
 package com.project_x.authentication.controller;
 
 import com.project_x.authentication.dto.request.ForgotPasswordOtpRequest;
+import com.project_x.authentication.dto.request.VerifyPasswordOtpRequest;
 import com.project_x.authentication.service.ForgotPasswordService;
 import com.project_x.core.response.ApiResponse;
+import com.project_x.verification.otp.dto.request.VerifyOtpRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,5 +25,10 @@ public class ForgotPasswordController {
     @PostMapping("/otp")
     public ResponseEntity<ApiResponse> sendForgotPasswordOtp(@RequestBody @Valid ForgotPasswordOtpRequest request){
         return forgotPasswordService.handleSendForgotPasswordOtp(request);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<ApiResponse> verifyForgotPasswordOtp(VerifyPasswordOtpRequest request){
+        return forgotPasswordService.handlePasswordOtpVerification(request);
     }
 }
