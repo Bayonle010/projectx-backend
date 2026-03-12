e813fb318e1254566f70f2f47c6443039cb899c9
Bayonle010 <afeezonadiran@gmail.com>
2026-03-12 10:20:44 +0100

    update: creates oauth2 entity

---
+++ src/main/java/com/project_x/authentication/socialauth/entity/Oauth2AuthenticationProvider.java
@@ -0,0 +1,37 @@
+package com.project_x.authentication.socialauth.entity;
+
+import com.project_x.user.entity.User;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+
+@Entity
+@Table(
+        name = "oauth2_authentication_providers",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_provider_type_provider_id", columnNames = {"provider_type", "provider_id"})
+        }
+)
+public class Oauth2AuthenticationProvider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "provider_type", nullable = false)
+    private Oauth2ProviderType providerType;
+
+    @Column(name = "provider_id", nullable = false)
+    private String providerId;
+
+    @Column(name = "provider_email")
+    private String providerEmail;
+
+    @Column(name = "email_verified")
+    private Boolean emailVerified;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
