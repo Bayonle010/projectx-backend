bd15e5d566415be5110688d6c1d674e0b3a01887
Bayonle010 <afeezonadiran@gmail.com>
2026-04-12 16:46:04 +0100

    update: models state and lga entity

---
+++ src/main/java/com/project_x/listing/adress/entity/Lga.java
@@ -0,0 +1,27 @@
+package com.project_x.listing.adress.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "lgas")
+public class Lga {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "state_id")
+    private State state;
+}
---
+++ src/main/java/com/project_x/listing/adress/entity/State.java
@@ -0,0 +1,23 @@
+package com.project_x.listing.adress.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "states")
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
