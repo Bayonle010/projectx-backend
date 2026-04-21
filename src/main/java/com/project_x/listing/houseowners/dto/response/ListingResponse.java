cd4fac5e29ffe5682270e487a8587a4b84e0aa67
Bayonle010 <afeezonadiran@gmail.com>
2026-04-21 16:11:49 +0100

    update: creates listing response

---
+++ src/main/java/com/project_x/listing/houseowners/dto/response/ListingResponse.java
@@ -0,0 +1,71 @@
+package com.project_x.listing.houseowners.dto.response;
+
+import com.project_x.listing.houseowners.enums.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+public record ListingResponse<ImageResponse>(
+
+        UUID id,
+
+        ListingRelationshipType relationshipType,
+        PropertyType propertyType,
+
+        Integer bedroomCount,
+        Integer bathroomCount,
+        Integer toiletCount,
+
+        PropertyCondition propertyCondition,
+        Integer unitCount,
+
+        String description,
+
+        WaterSource waterSource,
+
+        Boolean parkingAvailable,
+        Boolean fencedOrGated,
+        Boolean renovated,
+
+        FurnishingStatus furnishingStatus,
+
+        UUID stateId,
+        String stateName,
+
+        UUID lgaId,
+        String lgaName,
+
+        String addressLine,
+        String landmark,
+
+        Double latitude,
+        Double longitude,
+        String placeId,
+
+        Boolean shareAddressWithSeekers,
+
+        BigDecimal rentAmount,
+        RentPaymentFrequency rentPaymentFrequency,
+
+        BigDecimal agencyFee,
+        BigDecimal legalAgreementFee,
+        BigDecimal cautionFee,
+        BigDecimal serviceCharge,
+
+        String proofOfOwnershipUrl,
+
+        Set<String> amenities,
+
+        List<ImageResponse> images,
+
+        String videoUrl,
+
+        UUID ownerId,
+
+        Instant createdAt,
+        Instant updatedAt
+
+) {}
