31e0c08b6ed77b68b460697105a05fc620325661
Bayonle010 <afeezonadiran@gmail.com>
2026-04-21 16:15:23 +0100

    update: maps Listing entity to its repository

---
+++ src/main/java/com/project_x/listing/houseowners/repository/ListingRepository.java
@@ -0,0 +1,9 @@
+package com.project_x.listing.houseowners.repository;
+
+import com.project_x.listing.houseowners.entity.Listing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ListingRepository extends JpaRepository<Listing,UUID> {
+}
